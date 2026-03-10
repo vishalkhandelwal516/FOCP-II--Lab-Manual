8da5fa0d0b12e0f90b21b8b522ebe48f331c67b4
Vishal Khandelwal <158454202+vishalkhandelwal516@users.noreply.github.com>
2026-03-10 21:41:32 +0530

    Create Q24.cpp

---
+++ Q24.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(char i='A';i<='E';i++){
+        for(char j='A';j<=i;j++)
+            cout<<j;
+        cout<<endl;
+    }
+    return 0;
+}
