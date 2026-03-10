844d086b6d3f0b5fcfe9a75b59849db30cbeccb3
Vishal Khandelwal <158454202+vishalkhandelwal516@users.noreply.github.com>
2026-03-10 19:58:55 +0530

    Create Q8.cpp

---
+++ Q8.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if(i%3==0 && i%5==0)
+            cout<<"BuzzFuzz"<<endl;
+        else if(i%3==0)
+            cout<<"Buzz"<<endl;
+        else if(i%5==0)
+            cout<<"Fuzz"<<endl;
+        else
+            cout<<i<<endl;
+    }
+    return 0;
+}
